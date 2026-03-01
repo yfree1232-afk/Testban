16fd3c647599288b20afa8efa0d5a63b7d9400a3
yfree1232-afk <yfree1232@gmail.com>
2026-03-01 22:19:34 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Testban
+# Reporter-Telegram
+### A powerful tool for reporting channels, groups and accounts ⚠️
+
+### Menu :
+<img src="/report/Screenshot_20230805-113406_Pydroid 3.jpg">
+
+#### This tool uses the following methods to report :
+* Spam 🔆
+* Pornography 🔆
+* Violence 🔆
+* Child Abuse 🔆
+* Other 🔆
+* CopyRith 🔆
+* Fake 🔆
+* Geo Irrelevant 🔆
+* Illegal Drugs 🔆
+* Personal Details 🔆
+### Report Account
+<img src="/report/Screenshot_20230805-110416_Pydroid 3.jpg">
+
+### Report Channel
+<img src="/report/Screenshot_20230805-113258_Pydroid 3.jpg">
+
+### Report Group [updating]
+- It is currently being updated
+
+## tip
+
+- If you accidentally report something, your account will be reported! ❌
+- Report any violation you see accordingly
+- Any crime committed with this tool is the responsibility of the user ❌
+- This is a trial version! ♨️
+
+## Description
+
+### Modules used: 
+If installed , pip install {name}
+------------------------------------
+- telethon 🔰
+- prettytable 🔰
+- colorama (Required for Windows) 🤓
+------------------------------------
+
+## Support from
+- Linux ✅
+- Windows ✅
+- Termux ✅
+
+## Install :
+
+```
+git clone https://github.com/esfelurm/Reporter-Telegram
+cd Reporter-Telegram
+python main.py
+```
+### this 
+
+If you come across this text, the tool must be logged in once with your account in order to be able to run, enter your number and then enter the account login code. 👇🏻
+<img src="/report/20230805_120226.jpg">
+
+Goodbye my friends 
---
+++ main.py
@@ -0,0 +1,67 @@
+import time,os,platform
+from telethon import TelegramClient
+try:from prettytable import PrettyTable
+except:os.system("pip install prettytable")
+rd, gn, lgn, yw, lrd, be, pe = '\033[00;31m', '\033[00;32m', '\033[01;32m', '\033[01;33m', '\033[01;31m', '\033[94m', '\033[01;35m'
+cn, k,g = '\033[00;36m', '\033[90m','\033[38;5;130m'
+def re(text):
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(0.001)  
+if 'Windows' in platform.uname():
+    from colorama import init
+    init()
+else:
+    pass
+banner = f"""
+                                                                 
+{k}                                                                
+                              -     -                            
+                            .+       +.                          
+                           :#         #:                         
+                          =%           %-                        
+   {lrd} Telegram {k}  -{g} Jay Ghunawat{k}   .%+    {be} Max Reporter   {k}       
+                        #@:             -@#                      
+                     :  #@:             :@*  :                   
+                    -=  *@:             -@*  =-                  
+                   -%   *@-             =@+   %-                 
+                  -@=  .*@+             +@+.  =@-                
+                 =@%   .+@%-    :.:    -@@+.   #@:               
+                =@@#:     =%%-+@@@@@+-%%=     .#@@=              
+                 .+%@%+:.   -#@@@@@@@#-   .:=#@%=                
+                    -##%%%%%#*@@@@@@@*#%%%%%##-                  
+                  .*#######%@@@@@@@@@@@%#######*.                
+               .=#@%*+=--#@@@@@@@@@@@@@@@#--=+*%@#=.             
+            .=#@%+:     *@@@@@+.   .+@@@@@*     :+%@#=.          
+          :*@@=.    .=#@@@@@@@       @@@@@@@#=.    .=@@*.        
+            =@+    .%@@*%@@@@@*     *@@@@@%*@@%.    +@=          
+             :@=    +@# :@@@@@#     #@@@@%. #@+    =@:           
+              .#-   :@@  .%@@#       #@@#.  @@:   -#.            
+                +:   %@:   =%         %=   :@%   -+              
+                 -.  +@+                   +@+  .-               
+                  .  :@#                   #@:  .                
+                    @{cn}@Mannucybersecurity{k}@%                    
+                      :+@:               =@+:                    
+                        =@:             :@-                      
+                         -%.           .%:                       
+                          .#           #.                        
+                            +         +                          
+                             -       -                     
+"""
+
+re(banner)
+re("Warning ! This is a test reporter, any offense is the responsibility of the user !")
+print (f"{lrd}")
+t = PrettyTable([f'{cn}Number{lrd}',f'{cn}info{lrd}'])
+t.add_row([f'{lgn}1{lrd}',f'{gn}Reporter Channel{lrd}'])
+t.add_row([f'{lgn}2{lrd}',f'{gn}Reporter Account{lrd}'])
+t.add_row([f'{lgn}3{lrd}',f'{gn}Reporter Group [Updating]{lrd}'])
+print (t)
+
+number = input(f"{gn}Enter Number : {cn}")
+if number == "1":
+    os.system("python report/reporter.py")
+if number == "2":
+	os.system("python report/report.py")
+elif number == "3":
+    print ("This section is being updated and will be added soon \n\nChannel :@Mannucybersecurity")
